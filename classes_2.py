b6e8d866660134c3e51f503d220aaeacfdc9b43f
narjisse <narjisse.lasri@gmail.com>
2021-11-02 23:22:10 +0100

    feat : add walls

--- classes_2.py
+++ classes_2.py
@@ -59,6 +59,63 @@ class Area:
         self.coordinate_x = coordinate_x
         self.coordinate_y = coordinate_y
         self.elements = []
+        self.walls = np.zeros((4,1))
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
+
+    def get_coordinates(self):
+        return np.array([self.coordinate_x, self.coordinate_y, self.width, self.height])
+
+    def collision(self, new_element):
+        for element in self.elements:
+            if (new_element.coordinate_x + new_element.width > element.coordinate_x) and (new_element.coordinate_x < element.coordinate_x + element.width):
+                return True
+            elif (new_element.coordinate_x < element.coordinate_x + element.width) and (new_element.coordinate_x + new_element.width > element.coordinate_x):
+                return True
+            elif (new_element.coordinate_y + new_element.height > element.coordinate_y) and (new_element.coordinate_y < element.coordinate_y + element.height):
+                return True
+            elif (new_element.coordinate_y < element.coordinate_y + element.height) and (new_element.coordinate_y + new_element.height > element.coordinate_y):
+                return True
+            return False
+
+    def area_not_in_building(self, element):
+        if element.coordinate_x + element.width > self.width:
+            return True
+        elif element.coordinate_y + element.height > self.height:
+            return True
+        return False
+
+    def add_element(self, element):
+        if self.collision(element):
+            print("Error : there is already an area at this place")
+        elif self.area_not_in_building(element):
+            print("Error : The area is not in the building")
+        else :
+            self.areas.append(element)
+
+    def create_wall(self):
+        room_coordinates = self.get_coordinates()
+        room_coordinates_abs = room_coordinates[0]
+        room_coordinates_ord = room_coordinates[1]
+        room_coordinates_width = room_coordinates[2]
+        room_coordinates_height = room_coordinates[3]
+        self.walls[0] = Wall(room_coordinates_abs, room_coordinates_ord, room_coordinates_width, 0)
+        self.walls[1] = Wall(room_coordinates_abs, room_coordinates_ord, 0, room_coordinates_height)
+        self.walls[2] = Wall(room_coordinates_abs, room_coordinates_ord + room_coordinates_height, room_coordinates_width, 0)
+        self.walls[3] = Wall(room_coordinates_abs + room_coordinates_width, room_coordinates_ord, 0, room_coordinates_height)
+
+
+    
+class Element():
+    def __init__(self, coordinate_x, coordinate_y, width, height):
+        self.width = width
+        self.height = height
+        self.coordinate_x = coordinate_x
+        self.coordinate_y = coordinate_y
 
     def get_width(self):
         return self.width
@@ -68,7 +125,18 @@ class Area:
 
     def get_coordinate_x(self):
         return self.coordinate_x
-    
+
+    def get_coordinate_y(self):
+        return self.coordinate_y
+
+
+class Wall(Element):
+    def __init__(self, coordinate_x, coordinate_y, width, height):
+        self.width = width
+        self.height = height
+        self.coordinate_x = coordinate_x
+        self.coordinate_y = coordinate_y
+
 
 building = Building(20, 20, 2)
 room1 = Area(5,5, 5, 5)
